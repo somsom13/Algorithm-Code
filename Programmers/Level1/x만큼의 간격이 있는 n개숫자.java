7705ee67076aca7aa491d1ff4ffa6d32c5498fbc
gambas13 <somin1113@naver.com>
2021-01-13 16:03:25 +0900

    주석수정

--- Programmers/Level1/x만큼의 간격이 있는 n개숫자.java
+++ Programmers/Level1/x만큼의 간격이 있는 n개숫자.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long[] solution(long x, int n) { //주어진 문제조건에서 x는 -10000000 이상, 10000000 이하인 정수,n은 1000 이하인 자연수
+    public long[] solution(long x, int n) { 
         long[] answer = new long[n];
         for(int i=0;i<n;i++)
             answer[i]=x+i*x;
