9860ca2d4b498f19c26467ef47f63978e0bdd4aa
somsom13 <somin1113@naver.com>
2022-01-04 10:37:45 +0900

    백준 피보나치수5

---
+++ AlgorithmStudy_likelion/week8/10870.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 10870 {
+    // static Scanner sc=new Scanner(System.in);
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
+        int n= 0;
+        try {
+            n = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int res=fibo(n);
+        System.out.println(res);
+        return;
+    }
+
+    private static int fibo(int n){
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else{
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
+}
