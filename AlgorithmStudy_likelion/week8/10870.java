459adc17d9c4367db1ad82831996de23b424b327
somsom13 <somin1113@naver.com>
2022-01-05 22:58:38 +0900

    백준 재귀_피보나치수5

--- AlgorithmStudy_likelion/week8/10870.java
+++ AlgorithmStudy_likelion/week8/10870.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 10870 {
+public class Main {
     // static Scanner sc=new Scanner(System.in);
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args){
