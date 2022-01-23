d48cf85b7008438cf855fef158f671c1278836cf
somsom13 <somin1113@naver.com>
2022-01-23 17:32:14 +0900

    백준 큐, 덱_프린터 큐

---
+++ AlgorithmStudy_likelion/week10/1966.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+
+//각 케이스마다 해당 케이스 내 문서들의 우선순위가 담겨있어야함
+public class Main {
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException{
+        int n=Integer.parseInt(br.readLine());
+        Queue<int[]> queue;
+        PriorityQueue<Integer> priorities;
+
+
+        for(int i=0;i<n;i++){
+            String[] numsAndTarget=br.readLine().split(" "); // 문서 개수와 체크해야할 대상 문서의 인덱스
+            int len=Integer.parseInt(numsAndTarget[0]); //문서 개수
+            int needIdx=Integer.parseInt(numsAndTarget[1]); //체크 대상 문서 인덱스
+
+            queue=new LinkedList<>();
+            priorities=new PriorityQueue<>(Collections.reverseOrder());
+
+            // [원래 인덱스, 우선순위]
+            int[] s;
+            String[] inputs=br.readLine().split(" "); // inputs에 우선순위 순서대로 입력받음
+            for(int j=0;j<len;j++){
+                s=new int[2];
+                s[0]=j;
+                s[1]=Integer.parseInt(inputs[j]);
+                priorities.add(s[1]);
+                queue.add(s);
+            }
+
+
+            int count=0;
+            int maxVal=0;
+            if(!priorities.isEmpty()){
+                maxVal=priorities.peek();
+            }
+            
+            while(!queue.isEmpty()){
+                int[] current=queue.poll();
+                if(current[1]==maxVal){ //우선순위가 가장 크면? -> 바로 출력
+                    count++;
+                    if(current[0]==needIdx){ //출력한 애가 원하는 애랑 같으면? -> 비교 멈춤
+                        break;
+                    }
+
+                    priorities.poll(); // 출력한 문서는 대기열에서 삭제
+                    if(!priorities.isEmpty()){   
+                        maxVal=priorities.peek(); //최고 우선순위 갱신
+                    }
+                    continue;
+                }else{
+                    queue.add(current); //현재 문서 우선순위 < 대기열 모든 문서 중 최고 우선순위 -> 다음 문서 확인
+                }
+
+            }
+            bw.write(String.valueOf(count)+"\n");
+        }
+
+        bw.flush();
+    }
+}
